4fec9408d4e22cc7a69f7b7c9233741f9d4b1741
Nuradiyana <me@nooradiana.com>
2024-01-16 15:35:15 +0700

    Fixing compatible file `store` path

--- src/Actions/StoreModelUploadFile.php
+++ src/Actions/StoreModelUploadFile.php
@@ -27,7 +27,7 @@ final class StoreModelUploadFile
             'model_type' => $modelType,
             'file_name' => $uploadedFile->getClientOriginalName(),
             'storage_disk' => \config('filesystems.default'),
-            'file_path' => $uploadedFile->store(),
+            'file_path' => $uploadedFile->store('model-upload'),
             'state' => UploadFileState::upload,
         ]);
 
